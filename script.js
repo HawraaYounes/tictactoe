167893cd01b2995138f806c37987a35097559ab1
HawraaYounes <hawraayounes0@gmail.com>
2022-09-16 21:12:23 +0300

    Adding if statements to select win status

--- script.js
+++ script.js
@@ -4,9 +4,34 @@ const head=document.getElementById("head");
 const squares=[];//array that stores each square at each index
 function winner(){
     for(let i=1;i<10;i++){
-        squares[i]=document.getElementById('item+${id}');
-        console.log(squares[i])
+        squares[i]=document.getElementById('item'+i).innerHTML;
     }
+    if(squares[1]== squares[2] && squares[2]==squares[3] && squares[1]!=""){
+
+    }
+    else if(squares[4]== squares[5] && squares[5]==squares[6] && squares[5]!=""){
+
+    }
+    else if(squares[7]== squares[8] && squares[8]==squares[9] && squares[9]!=""){
+
+    }
+    else if(squares[1]== squares[4] && squares[4]==squares[7] && squares[7]!=""){
+
+    }
+    else if(squares[2]== squares[5] && squares[5]==squares[8] && squares[8]!=""){
+
+    }
+    else if(squares[3]== squares[6] && squares[6]==squares[9] && squares[9]!=""){
+
+    }
+    else if(squares[1]== squares[5] && squares[5]==squares[9] && squares[9]!=""){
+
+    }
+    else if(squares[3]== squares[5] && squares[5]==squares[7] && squares[7]!=""){
+
+    }
+    
+
 }
 
 
