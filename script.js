f1d87ad2059459defd6b66721686c860a47c4316
HawraaYounes <hawraayounes0@gmail.com>
2022-09-16 20:12:29 +0300

    Switching turns between red and yellow

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let turn='x';
-
+let turn='red';
+const head=document.getElementById("head");
 function playGame(id){
     let square=document.getElementById(id);
-    if(turn='x'){
-        turn='o';
-        square.add
-
+    if(turn==='red' && square.innerHTML==" "){
+        turn='yellow';
+        square.classList.add("red");
+        head.innerHTML("Hey Yellow! It's your turn ")
     }
+    else if(turn==='yellow' && square.innerHTML==" "){
+        turn='red';
+        square.classList.add("yellow");
+        head.innerHTML("Hey Red! It's your turn ")
+    }
+
 }
