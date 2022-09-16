78bd9688815d88b70761007301caed5500caea6e
HawraaYounes <hawraayounes0@gmail.com>
2022-09-16 20:18:33 +0300

    Editing squares style

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let turn='red';
 const head=document.getElementById("head");
 function playGame(id){
     let square=document.getElementById(id);
-    if(turn==='red' && square.innerHTML==" "){
+    if(turn==='red' && square.innerHTML==""){
         turn='yellow';
         square.classList.add("red");
         head.innerHTML("Hey Yellow! It's your turn ")
     }
-    else if(turn==='yellow' && square.innerHTML==" "){
+    else if(turn==='yellow' && square.innerHTML==""){
         turn='red';
         square.classList.add("yellow");
         head.innerHTML("Hey Red! It's your turn ")
